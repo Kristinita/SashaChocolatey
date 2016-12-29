459701e34e42757c5ad14f0812c3710e71cf41db
Kristinita <SashaChernykhEmpressOfUniverse@kristinita.ru>
2016-12-29 20:36:25 +0300

    Add Anvide Seal Folder

---
+++ packages/Anvide_Seal_Folder/tools/chocolateyinstall.ps1
@@ -0,0 +1,20 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName= 'Anvide Seal Folder'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'http://anvidelabs.org/download/AnvideSealFolder.exe'
+$url64      = ''
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  url           = $url
+  url64bit      = $url64
+
+  softwareName  = 'Anvide Seal Folder*'
+
+  silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+}
+
+Install-ChocolateyPackage @packageArgs
