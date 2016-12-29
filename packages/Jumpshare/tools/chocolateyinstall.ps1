c68a95aa72a95e395ad8afbd50a5580b66030043
Kristinita <SashaChernykhEmpressOfUniverse@kristinita.ru>
2016-12-29 18:42:27 +0300

    1. Add Jumpshare, 2. Change irc to dc tag

---
+++ packages/Jumpshare/tools/chocolateyinstall.ps1
@@ -0,0 +1,25 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName= 'Jumpshare'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://d21hi1or3tbtjm.cloudfront.net/desktop/windows/JumpshareInstaller-2.0.9.exe'
+$url64      = ''
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  url           = $url
+  url64bit      = $url64
+
+  softwareName  = 'Jumpshare*'
+
+  checksum      = ''
+  checksumType  = 'sha256'
+  checksum64    = ''
+  checksumType64= 'sha256'
+
+  silentArgs   = '/S'
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ packages/Jumpshare/tools/chocolateyuninstall.ps1
@@ -0,0 +1,31 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'Jumpshare'
+$softwareName = 'Jumpshare*'
+$installerType = 'EXE'
+
+if ($installerType -ne 'MSI') {
+  $silentArgs = '/S'
+  $validExitCodes = @(0)
+}
+
+$uninstalled = $false
+[array]$key = Get-UninstallRegistryKey -SoftwareName $softwareName
+
+if ($key.Count -eq 1) {
+  $key | % {
+    $file = "$($_.UninstallString)"
+
+    if ($installerType -eq 'MSI') {
+      $silentArgs = "$($_.PSChildName) $silentArgs"
+
+      $file = ''
+    }
+
+    Uninstall-ChocolateyPackage -PackageName $packageName `
+                                -FileType $installerType `
+                                -SilentArgs "$silentArgs" `
+                                -ValidExitCodes $validExitCodes `
+                                -File "$file"
+  }
+}
