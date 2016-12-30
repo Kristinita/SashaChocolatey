845e3849cbb33f8de8068534ea1ebc59c263bbd8
Kristinita <SashaChernykhEmpressOfUniverse@kristinita.ru>
2016-12-30 21:21:57 +0300

    Add SmillaEnlarger

---
+++ packages/SmillaEnlarger/tools/chocolateyinstall.ps1
@@ -0,0 +1,5 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyZipPackage -PackageName 'SmillaEnlarger' -Url 'http://downloads.sourceforge.net/project/imageenlarger/imageenlarger/SmillaEnlarger%20Release%200.9.0/SmillaEnlarger-0.9.0.zip' -UnzipLocation $toolsDir
