79ebe120bb9c91b627f2f25897ba69348ed8d400
Kristinita <SashaChernykhEmpressOfUniverse@kristinita.ru>
2017-01-04 10:01:48 +0300

    Fixed Deimos Mouse Driver

--- packages/Deimos Mouse Driver/tools/chocolateyinstall.ps1
+++ packages/Deimos Mouse Driver/tools/chocolateyinstall.ps1
@@ -1,5 +1,23 @@
 $ErrorActionPreference = 'Stop';
 
+$packageName= 'Deimos Mouse Driver'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'http://www1.canyon.eu/files/downloads/drivers/cnd-sgm4n.zip'
+$url64      = ''
 
-Install-ChocolateyZipPackage -PackageName 'Deimos Mouse Driver' -Url 'http://www1.canyon.eu/files/downloads/drivers/cnd-sgm4n.zip' -UnzipLocation $toolsDir
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  url           = $url
+  url64bit      = $url64
+
+  softwareName  = 'Deimos Mouse Driver*'
+
+  checksum        = '8F6987E23C20F7764D309F7F6E2FE334374F6BB4D39EF23E806CE7396F31BFFF'
+  checksumType    = 'sha256'
+  checksum64      = '8F6987E23C20F7764D309F7F6E2FE334374F6BB4D39EF23E806CE7396F31BFFF'
+  checksumType64  = 'sha256'
+
+}
+
+Install-ChocolateyZipPackage @packageArgs
