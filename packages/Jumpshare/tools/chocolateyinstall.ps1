76d115a0cdf56c609856f49717504756a7d9ac50
Kristinita <SashaChernykhEmpressOfUniverse@kristinita.ru>
2017-02-16 14:07:25 +0300

    [Upgrade] Jumpshare to 2.0.10

--- packages/Jumpshare/tools/chocolateyinstall.ps1
+++ packages/Jumpshare/tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@ $ErrorActionPreference = 'Stop';
 
 $packageName= 'Jumpshare'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://d21hi1or3tbtjm.cloudfront.net/desktop/windows/JumpshareInstaller-2.0.9.exe'
+$url        = 'https://d21hi1or3tbtjm.cloudfront.net/desktop/windows/JumpshareInstaller-2.0.10.exe'
 $url64      = ''
 
 $packageArgs = @{
@@ -14,9 +14,9 @@ $packageArgs = @{
 
   softwareName  = 'Jumpshare*'
 
-  checksum      = 'DBE17A25C6FE8E60662FF18502752CB7F2D780E1AE91928F4C4D41CE627FDEFE'
+  checksum      = 'D2C408437D4706EF797E8976C7C20FD9276958D8AA8BA90021A9C5124E5DCB91'
   checksumType  = 'sha256'
-  checksum64    = 'DBE17A25C6FE8E60662FF18502752CB7F2D780E1AE91928F4C4D41CE627FDEFE'
+  checksum64    = ''
   checksumType64= 'sha256'
 
   silentArgs   = '/S'
