d275a13567dc392d7eae4aef6a46a52ea9250294
Kristinita <SashaChernykhEmpressOfUniverse@kristinita.ru>
2017-03-05 07:37:18 +0300

    [Add] Wox

---
+++ packages/Wox/tools/chocolateyInstall.ps1
@@ -0,0 +1,35 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'Wox'
+$toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url         = 'https://github.com/Wox-launcher/Wox/releases/download/v1.3.183/Wox-1.3.183.exe'
+$url64       = ''
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  url           = $url
+  url64bit      = $url64
+
+  checksum      = '006B6C8922FE9D8FEC4D3EF42DAF7D6296C181F0967A856B2BB22F69D2E3BE5E'
+  checksumType  = 'sha256'
+  checksum64    = ''
+  checksumType64= 'sha256'
+
+  ######
+  # EXE
+  ######
+  # silentArgs   = '/S'           # NSIS
+  # silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' # Inno Setup
+  # silentArgs   = '/s'           # InstallShield
+  # silentArgs   = '/s /v"/qn"'   # InstallShield with MSI. Yes, use "quotes" after «v» and «qn», it not typos.
+  # silentArgs   = '/s'           # Wise InstallMaster
+  # silentArgs   = '-s'           # Squirrel
+  # silentArgs   = '-q'           # Install4j
+  # silentArgs   = '-s -u'        # Ghost
+
+  silentArgs   = '/S'
+}
+
+Install-ChocolateyPackage @packageArgs
