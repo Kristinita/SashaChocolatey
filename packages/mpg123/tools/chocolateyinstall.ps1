48f894ea503c3895da8b04ed812431dff9ea4d3c
Kristinita <SashaChernykhEmpressOfUniverse@kristinita.ru>
2017-04-30 11:14:49 +0300

    [Add] mpg123

--- packages/mpg123/tools/chocolateyinstall.ps1
+++ packages/mpg123/tools/chocolateyinstall.ps1
@@ -2,11 +2,11 @@
 $ErrorActionPreference = 'Stop';
 
 # Insert here full correct name of program, not Chocolatey package name. For example, correct «Performance Maintainer», not «pername».
-$packageName = 'X-Fire'
+$packageName = 'mpg123'
 $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 # Don't forget specify protocol for URL's!
-$url         = 'https://github.com/Kristinita/SashaChocolatey/raw/master/installation-files/X-Fire/LightSashaFire.CursorPack'
-$url64       = ''
+$url         = 'https://www.mpg123.de/download/win32/1.24.0/mpg123-1.24.0-static-x86.zip'
+$url64       = 'https://www.mpg123.de/download/win64/1.24.0/mpg123-1.24.0-static-x86-64.zip'
 
 $packageArgs = @{
   packageName   = $packageName
@@ -15,9 +15,9 @@ $packageArgs = @{
   url           = $url
   url64bit      = $url64
 
-  checksum        = '6C83251DFA0FEB664EE8DC0C340E9383C177C7D12CB367A994311C936F233EEA'
+  checksum        = 'C0F74CE130075F8D544B3EAD85535508AB239B855C16F54FDD74CD17571D559F'
   checksumType    = 'sha256'
-  checksum64      = ''
+  checksum64      = '0E6ACF7EFB930AA10693FCAFCC1D4A02BE82C1E46A5CF77D2908809D7325BC2D'
   checksumType64  = 'sha256'
 
 }
