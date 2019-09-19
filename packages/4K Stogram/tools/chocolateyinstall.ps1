3c553e81df977e80ebe0fe76f91da8bca3606a62
TheCakeIsNaOH <38865330+TheCakeIsNaOH@users.noreply.github.com>
2019-09-19 17:37:39 -0500

    4k-stogram to 2.7.3

--- packages/4K Stogram/tools/chocolateyinstall.ps1
+++ packages/4K Stogram/tools/chocolateyinstall.ps1
@@ -1,25 +1,25 @@
 $ErrorActionPreference = 'Stop';
-
-$packageName= '4K Stogram'
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://downloads.4kdownload.com/app/4kstogram_2.2.exe'
-$url64      = ''
+$packageName		   = '4K Stogram'
+$toolsDir  			   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://dl.4kdownload.com/app/4kstogram_2.7.3.msi?source=chocolatey'
+$url64      = 'https://dl.4kdownload.com/app/4kstogram_2.7.3_x64.msi?source=chocolatey'
 
 $packageArgs = @{
   packageName   = $packageName
   unzipLocation = $toolsDir
-  fileType      = 'exe'
+  fileType      = 'MSI'
   url           = $url
   url64bit      = $url64
 
   softwareName  = '4K Stogram*'
 
-  checksum      = 'AFE3DC6E739AC41F959D47BA4C0322E723CC820786B01B09C82EB6A13C0A6A16'
+  checksum      = '22800ee03f54d249ef5eed1fa4298345b42b09e5efa98b4d2e7473465bb0f341'
   checksumType  = 'sha256'
-  checksum64    = ''
+  checksum64    = '21d47632a2222ee92dd8ad2e14a1d4544335a800900e3324313a55ad1acf5a24'
   checksumType64= 'sha256'
 
-  silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  validExitCodes= @(0, 3010, 1641)
 }
 
 Install-ChocolateyPackage @packageArgs
