71ad2998d59e96b6cfa5b5c3374e82c92caabb36
Chris Chinchilla <chris@chrischinchilla.com>
2020-09-04 22:03:55 +0200

    feat(update): Vale to 2.3.4
    * [upgrade] Vale to 2.3.4
    
    Remove extraneous variables as requested by reviewers
    
    * Fixed versions in link

--- packages/Vale/tools/chocolateyinstall.ps1
+++ packages/Vale/tools/chocolateyinstall.ps1
@@ -2,18 +2,14 @@ $ErrorActionPreference = 'Stop';
 
 $packageName = 'Vale'
 $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url         = ''
-$url64       = 'https://github.com/errata-ai/vale/releases/download/v2.3.1/vale_2.3.1_Windows_64-bit.zip'
+$url64       = 'https://github.com/errata-ai/vale/releases/download/v2.3.4/vale_2.3.4_Windows_64-bit.zip'
 
 $packageArgs = @{
   packageName   = $packageName
   unzipLocation = $toolsDir
-  url           = $url
   url64bit      = $url64
 
-  checksum        = ''
-  checksumType    = ''
-  checksum64      = 'd91b5e82ddbff5ba7de24b2275fc35e4d7f53988a8bf0e563e1178bb56d3f24b'
+  checksum64      = 'd7c314ca9fe8d5a693aa79caa90c8f70c179b65766a594cd3bfa5b7e5e1a89c5'
   checksumType64  = 'sha256'
 
 }
