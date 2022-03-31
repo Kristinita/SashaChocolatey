a36f8edbfc2760310431d3d907cfd8f0d93a9922
jdkato <joseph@jdkato.io>
2022-03-30 18:16:14 -0700

    feat(update): Vale to 2.15.5

--- packages/Vale/tools/chocolateyinstall.ps1
+++ packages/Vale/tools/chocolateyinstall.ps1
@@ -2,14 +2,14 @@ $ErrorActionPreference = 'Stop';
 
 $packageName = 'Vale'
 $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64       = 'https://github.com/errata-ai/vale/releases/download/v2.15.4/vale_2.15.4_Windows_64-bit.zip'
+$url64       = 'https://github.com/errata-ai/vale/releases/download/v2.15.5/vale_2.15.5_Windows_64-bit.zip'
 
 $packageArgs = @{
   packageName   = $packageName
   unzipLocation = $toolsDir
   url64bit      = $url64
 
-  checksum64      = '5c29301f1c953ded48fc3daefa25e27596098665769ecf3900f9c133c2a743f0'
+  checksum64      = '5b4da9b1c04404547287195fa453f1bddf2bebdb608319dc4295801f9d9a8c22'
   checksumType64  = 'sha256'
 
 }
